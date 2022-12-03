f58d89b43f2b6f62a0d16a054719ca38a53b0086
Jennifer McCain <111464785+F1REW8LL@users.noreply.github.com>
2022-12-02 20:49:28 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -375,75 +375,33 @@ public class Main extends Application {
     launch(args);
   }
   
-  ObservableList<Player> players = FXCollections.observableArrayList();
-  public ObservableList<Player> getPlayers() { // Gets all the players
-    players.addAll(new Player("Player 1", "Team 1"),
-	               new Player("Player 2", "Team 2"),
-	               new Player("Player 3", "Team 3"),
-	               new Player("Player 4", "Team 4"),
-	               new Player("Player 5", "Team 5"));
-    return players;
-  }
-  
-  ObservableList<Player> pitchers = FXCollections.observableArrayList();
-  public ObservableList<Player> getPitchers() { // Gets the new team
-    players.addAll(new Player("Pitcher 1", "Team 1"),
-	               new Player("Pitcher 2", "Team 2"),
-	               new Player("Pitcher 3", "Team 3"),
-	               new Player("Pitcher 4", "Team 4"),
-	               new Player("Pitcher 5", "Team 5"));
-    return pitchers;
-  }
-  // Function here
-  
   @Override
   public void start(Stage stage) throws Exception {
-    // Select Players Screen - scene1
-
     // Player List
     Text playerListText = new Text("Player List"); // Make text
-    Text playerListColumns = new Text("Player      Team");
-    ListView playerList = new ListView(); // Make a list
-    ListView pitcherList = new ListView();
+    Text playerListColumns = new Text("Position	        Name);
+    ListView playerList = new ListView(players); // Make a list ***SCOPE ERROR***
+    ListView pitcherList = new ListView(pitchers);
     VBox column1 = new VBox(); // Make column 1 of scene1
     column1.getChildren().addAll(playerListText, playerListColumns, playerList); // Add label and table to column 1
     
-    // Drop Down Menu
-    ChoiceBox<String> positionMenu = new ChoiceBox<>(); // Make a drop down menu
-    ObservableList<String> positionNames = positionMenu.getItems(); // Make observable list
-    positionNames.addAll("Catcher", "Pitcher", "First Base", "Second Base", "Third Base",
-               "Shortstop", "Left Fielder", "Right Fielder", "Center Fielder");
-    String test[] = new String[] {
-        "Catchers", "Pitchers", "First Base", "Second Base", "Third Base",
-        "Shortstop", "Left Fielder", "Right Fielder", "Center Fielder"};
-    Text testText = new Text("TEST TEXT");    
-    positionMenu.getSelectionModel().selectedIndexProperty().addListener( // Add action to the choice box
-        (ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
-         testText.setText(test[new_val.intValue()]); });    
-    
     // Buttons
-//    positionMenu.getItems().addAll("Position 1", "Position 2", "Position 3", "Position 4", // Add options
-//                     "Position 5", "Position 6", "Position 7", "Position 8");
-    positionMenu.setValue("Position Menu"); // Set default text for drop down menu
     Button addButton = new Button("Add Player"); // Make buttons
     Button removeButton = new Button("Remove Player");
     Button finishButton = new Button("Finish Team");
     
     VBox column2 = new VBox(); // Make column 2 of scene1
-    column2.getChildren().addAll(positionMenu, addButton, removeButton, finishButton); // Add buttons to column 2
+    column2.getChildren().addAll(addButton, removeButton, finishButton); // Add buttons to column 2
     column2.setSpacing(30);
     column2.setMargin(addButton, new Insets(40, 0, 0, 0)); // Set margins
     column2.setMargin(removeButton, new Insets(0, 0, 40, 0));    
     
-    playerList.getItems().addAll(getPlayers());
-    pitcherList.getItems().addAll(getPitchers());
-    
     // Team List
     Text teamListText = new Text("Team List"); 
-    Text teamListColumns = new Text("Player      Team");
-    ListView teamList = new ListView();
+    Text teamListColumns = new Text("Name            Position");
+    ListView teamList = new ListView(myTeam.getFirstBase, myTeam.getSecondBase, myTeam.getThirdBase, myTeam.getShortStop, myTeam.getOutFields, myTeam.getDesighit);
     VBox column3 = new VBox(); // Make column 3 of scene1
-    column3.getChildren().addAll(teamListText, teamList); // Add label and list to column 3
+    column3.getChildren().addAll(teamListText, teamListColumns, teamList); // Add label and list to column 3
     
     // Page #1 Layout
     HBox layout1 = new HBox(); // Make an HBox layout
@@ -456,37 +414,39 @@ public class Main extends Application {
     // Pitcher Column
     Text pitcherStatsText = new Text("Pitcher Statistics");
     Text pitcherStatsColumns = new Text("Player      Score");
-    ListView pitcherStats = new ListView();
+    ListView pitcherStatsList = new ListView(pitcherStats);
     VBox pitcherColumn = new VBox(); // Left side of scene2
-    pitcherColumn.getChildren().addAll(pitcherStatsText, pitcherStatsColumns, pitcherStats);
+    pitcherColumn.getChildren().addAll(pitcherStatsText, pitcherStatsColumns, pitcherStatsList);
     
     // Batter Column
     Text batterStatsText = new Text("Batter Statistics");
-    Text batterStatsColumns = new Text("Player      Score");
-    ListView batterStats = new ListView();
+    Text batterStatsColumns = new Text("Player          Score");
+    ListView batterStatsList = new ListView(batterStats);
     VBox batterColumn = new VBox();
-    batterColumn.getChildren().addAll(batterStatsText, batterStatsColumns, batterStats);
+    batterColumn.getChildren().addAll(batterStatsText, batterStatsColumns, batterStatsList);
     
     // Bottom: Buttons and Points
-    Button buttonEdit = new Button("Edit"); // Make buttons
-    Button buttonReset = new Button("Reset");
-    String points = "Points: "; // + getPoints();
+    Button editButton = new Button("Edit"); // Make buttons
+    Button resetButton = new Button("Reset");
+    String points = "Points: " + getPoints();
     Text pointsText = new Text(points);
     
     // Page #2 Layout
     VBox layout2 = new VBox(); // Contains columns and bottomPane
     HBox columns = new HBox();
     HBox bottomPane = new HBox(); // Contains
-    bottomPane.getChildren().addAll(buttonEdit, pointsText, buttonReset); // Add buttons to bottom node
+    bottomPane.getChildren().addAll(editButton, pointsText, resetButton); // Add buttons to bottom node
     bottomPane.setSpacing(50);
     
     columns.getChildren().addAll(batterColumn, pitcherColumn); // Add tables and labels to the columns HBox
     layout2.getChildren().addAll(columns, bottomPane); // Add columns to the layout2 VBox
     
     Scene scene2 = new Scene(layout2, 1000, 700); // Make second scene
+    addButton.setOnAction(e -> myTeam.addPlayer(playerList.getSelectionModel().getSelectedItem()));
+    removeButton.setOnAction(e -> myTeam.removePlayer(teamList.getSelectionModel().getSelectedItem()));
     finishButton.setOnAction(e -> stage.setScene(scene2)); // Make "Finish Team" button go to Score Screen (scene2)
-    buttonEdit.setOnAction(e -> stage.setScene(scene1)); // Make "Edit" button go to Select Players Screen (scene1)
-    buttonReset.setOnAction(e -> stage.setScene(scene1)); // Make "Reset" button go to Select Players Screen (scene1)
+    editButton.setOnAction(e -> stage.setScene(scene1)); // Make "Edit" button go to Select Players Screen (scene1)
+    resetButton.setOnAction(e -> stage.close()); // Make "Reset" button go to Select Players Screen (scene1)
     
     String css = this.getClass().getResource("application.css").toExternalForm();
     scene1.getStylesheets().add(css); // Add CSS
@@ -536,7 +496,7 @@ public class Main extends Application {
         }
     });
     
-    pitcherStats.setCellFactory(param -> new ListCell<Player>() {
+    pitcherStatsList.setCellFactory(param -> new ListCell<Player>() {
         @Override
         protected void updateItem(Player item, boolean empty) {
             super.updateItem(item, empty);
@@ -549,7 +509,7 @@ public class Main extends Application {
 	        }
 	    });
     
-    batterStats.setCellFactory(param -> new ListCell<Player>() {
+    batterStatsList.setCellFactory(param -> new ListCell<Player>() {
         @Override
         protected void updateItem(Player item, boolean empty) {
             super.updateItem(item, empty);
@@ -570,7 +530,7 @@ public class Main extends Application {
     pitcherStatsText.setFont(Font.font("Century Gothic", 30));
     pitcherStatsColumns.setFont(Font.font("Century Gothic", 20));
     batterStatsText.setFont(Font.font("Century Gothic", 30));
-    batterStatsColumns.setFont(Font.font("C  entury Gothic", 20));
+    batterStatsColumns.setFont(Font.font("Century Gothic", 20));
     pointsText.setFont(Font.font("Century Gothic", 20));
     
     // Set margins
